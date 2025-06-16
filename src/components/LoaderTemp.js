22128b498807e3c03785434f62acd792ae0be0c1
Vartika Lohiya <vartikalohiya7@gmail.com>
2025-06-16 18:28:05 +0530

    feat: Add animated page loader for smooth transitions

--- src/components/LoaderTemp.js
+++ src/components/LoaderTemp.js
@@ -4,7 +4,7 @@ import './Loader.css';
 const Loader = () => {
   return (
     <div className="loader-wrapper">
-      <img src="/truck2.gif" alt="Loading..." className="loader-truck" />
+      <img src="/truck2.gif" alt="Loading animation" className="loader-truck" />
       <h2 className="loader-text">Vipreshana...</h2>
     </div>
   );
