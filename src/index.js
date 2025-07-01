3e90e2e92e59291c635a6a8cf3c44ef71c47c459
RinkiJhunjhunwala <jhunjhunwalarinki@gmail.com>
2025-07-01 14:18:14 +0530

    Final

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { ThemeProvider } from './context/ThemeContext';
 
 // Set the document title 
 document.title = 'Vipreshana';
