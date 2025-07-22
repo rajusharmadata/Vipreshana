96667cd4710bf5fef6cf7f3139beb7d9ea3deceb
Himanshu Kabra <kabrahimanshu123@gmail.com>
2025-07-22 18:49:25 +0530

    bug fixes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,10 +3,12 @@ import { FaGithub, FaLinkedin, FaEnvelope } from 'react-icons/fa';
 
 const Footer = () => {
     return (
-        <footer className="pt-8 pb-6 px-4 md:px-12 bg-gray-800 text-gray-300">
-            <div className="max-w-6xl mx-auto flex flex-col md:flex-row items-center justify-between space-y-4 md:space-y-0">
-                <p className="text-sm">&copy; {new Date().getFullYear()} Vipreshana. All rights reserved.</p>
-                <div className="flex space-x-6 text-xl">
+        <footer className="pt-6 pb-6 px-4 md:px-12 bg-gray-800 text-gray-300">
+            <div className="max-w-6xl mx-auto relative flex flex-col items-center md:items-stretch">
+                <p className="text-sm text-center w-full">
+                    &copy; {new Date().getFullYear()} Vipreshana. All rights reserved.
+                </p>
+                <div className="flex space-x-6 text-xl mt-0 md:mt-3 md:absolute md:top-1/2 md:right-0 md:transform md:-translate-y-1/2">
                     <a
                         href="https://github.com/sailaja-adapa/"
                         target="_blank"
